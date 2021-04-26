a122e13956670e8d84b030be26073a3b10cf4aa8
AzatG <>
2021-04-26 16:12:08 +0000

    Blatt.2 Aufgabe.2

--- stichprobe2.cc
+++ stichprobe2.cc
@@ -37,17 +37,26 @@ int main()
   double varianz = 0;
 
   std::ifstream fin("datensumme.txt");
-  std::ofstream fout("mittelwert.txt");
+  std::ofstream fout("mittelwerte.txt");
+  std::ifstream gin("datensumme.txt");
+  std::ofstream gout("varianzen.txt");
 
   for (int i=1; i<=26; ++i)
   {
     for (int j=1; j<=9; ++j)
     {
       fin >>a ;
-      summe += a;  
-    }
+      summe += a;   
+    }  
     mittelwert = summe/9;
     summe = 0;
+    for (int k=1; k<=9; ++k)
+    {
+      gin >>a ;
+      varianz += (a - mittelwert)*(a - mittelwert);
+    }
+    varianz /= 9; 
     fout << mittelwert << std::endl;
+    gout << varianz << std::endl;
   }
 }
