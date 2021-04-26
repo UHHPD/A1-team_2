151dec6ed1d673340f78f8b393abd7a7c40f67c5
AzatG <>
2021-04-26 15:35:33 +0000

    Blatt.2 Aufgabe.1

--- stichprobe.cc
+++ stichprobe.cc
@@ -4,12 +4,13 @@
 
 int main()
 {
-  float a, mittelwert;
-  float summe = 0;
-  float varianz = 0;
+  int a; 
+  double mittelwert;
+  double summe = 0;
+  double varianz = 0;
 
   std::ifstream fin("datensumme.txt");
-  for (int i=0; i<= 234; ++i)
+  for (int i=0; i< 234; ++i)
   {
     fin >>a ;
     summe += a;
@@ -17,7 +18,7 @@ int main()
   mittelwert = summe/234;
   fin.close();
   std::ifstream gin("datensumme.txt");
-  for (int i=0; i<= 234; ++i)
+  for (int i=0; i< 234; ++i)
   {
     gin >>a ;
     varianz += (a - mittelwert)*(a - mittelwert);
